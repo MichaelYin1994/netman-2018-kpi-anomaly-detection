e1bde577a875f04f45c3a86a6ef1dcd666041c02
MichaelYin1994 <zhuoyin94@163.com>
2021-09-13 15:34:02 +0800

    (feat) 特征工程由离线转基于StreamDeque的实时形式

--- src/compute_feature_engineering.py
+++ src/compute_feature_engineering.py
@@ -26,219 +26,77 @@ import pandas as pd
 from numba import njit
 from tqdm import tqdm
 
-from utils import LoadSave
+from utils import LoadSave, StreamDeque
 
 # 设定全局随机种子，并且屏蔽warnings
 GLOBAL_RANDOM_SEED = 2021
 np.random.seed(GLOBAL_RANDOM_SEED)
 warnings.filterwarnings('ignore')
 ###############################################################################
-@njit
-def njit_sliding_window_mean(timestamp, sensor_vals, window_size):
-    '''以one-pass的方式计算array的每一个元素给定window_size内的mean。'''
-    if not (len(timestamp) == len(sensor_vals)):
-        raise ValueError(
-            'Timestamp array size mismatch with the sensor_vals array !'
-        )
 
-    # 滚动计算每一时刻给定时间窗口内的统计量
-    window_mean_vals = np.zeros((len(timestamp), ), dtype=np.float32)
-    window_cum_sum, window_count = 0, 0
-
-    front = 0
-    for rear in range(len(timestamp)):
-        seconds_gap = timestamp[rear] - timestamp[front]
-
-        window_cum_sum += sensor_vals[rear]
-        window_count += 1
-
-        # 若是头尾指针窗口时间差不满足范围，则收缩窗口
-        if seconds_gap > window_size:
-            while(front <= rear and seconds_gap > window_size):
-                window_count -= 1
-                window_cum_sum -= sensor_vals[front]
-
-                front += 1
-                seconds_gap = timestamp[rear] - timestamp[front]
-        window_mean_vals[rear] = window_cum_sum / window_count
-
-    return window_mean_vals
-
-
-@njit
-def njit_sliding_window_std(timestamp, sensor_vals, window_size):
-    '''以one-pass的方式计算array的每一个元素给定window_size内的std。'''
-    if not (len(timestamp) == len(sensor_vals)):
-        raise ValueError(
-            'Timestamp array size mismatch with the sensor_vals array !'
-        )
-
-    # 滚动计算每一时刻给定时间窗口内的统计量
-    window_std_vals = np.zeros((len(timestamp), ), dtype=np.float32)
-    window_cum_sum, window_cum_sum_square, window_count = 0, 0, 0
-
-    front = 0
-    for rear in range(len(timestamp)):
-        seconds_gap = timestamp[rear] - timestamp[front]
-
-        window_cum_sum += sensor_vals[rear]
-        window_cum_sum_square += sensor_vals[rear]**2
-        window_count += 1
-
-        # 若是头尾指针窗口时间差不满足范围，则收缩窗口
-        if seconds_gap > window_size:
-            while(front <= rear and seconds_gap > window_size):
-                window_count -= 1
-                window_cum_sum -= sensor_vals[front]
-                window_cum_sum_square -= sensor_vals[front]**2
-
-                front += 1
-                seconds_gap = timestamp[rear] - timestamp[front]
-
-        window_std_vals[rear] = window_cum_sum_square / window_count - \
-            (window_cum_sum / window_count)**2
-        window_std_vals[rear] = np.sqrt(window_std_vals[rear])
-
-    return window_std_vals
-
-
-def njit_sliding_window_weighted_moving_mean(
-        timestamp, sensor_vals, window_size, interval
-    ):
-    '''以one-pass的方式计算array的每一个元素给定window_size内的WMA。'''
-    if not (len(timestamp) == len(sensor_vals)):
-        raise ValueError(
-            'Timestamp array size mismatch with the sensor_vals array !'
-        )
-
-    # 滚动计算每一时刻给定时间窗口内的统计量
-    pass
-
-
-def njit_sliding_window_exponentially_weighted_moving_mean():
-    pass
-
-
-
-def njit_sliding_window_max():
-    '''以stream形式，以one-pass的方式计算array的每一个元素给定window_size内的max。'''
-    pass
-
-
-def njit_sliding_window_min():
-    pass
-
-
-
-def njit_sliding_window_skew():
-    pass
-
-
-def njit_sliding_window_kurtosis():
-    pass
-
-
-@njit
-def njit_time_shift(timestamp, sensor_vals, last_seconds):
-    '''以one-pass的方式计算array的每一个元素给定offline_inference内的shift。
-    并采用LLVM编译器进行加速。
-    '''
-    if not (len(timestamp) == len(sensor_vals)):
-        raise ValueError(
-            'Timestamp array size mismatch with the sensor_vals array !'
-        )
-
-    # 滚动计算shift
-    shifted_vals = np.zeros((len(timestamp), ), dtype=np.float32)
-
-    front = 0
-    for rear in range(len(timestamp)):
-        seconds_gap = timestamp[rear] - timestamp[front]
-
-        # 若是头尾指针窗口时间差不满足范围，则收缩窗口
-        if seconds_gap > last_seconds:
-            while(front < rear and seconds_gap > last_seconds):
-                front += 1
-                seconds_gap = timestamp[rear] - timestamp[front]
-        shifted_vals[rear] = sensor_vals[front]
-
-    return shifted_vals
-
-
-def compute_feature_engineering_single_kpi(df=None):
-    '''计算给定KPI曲线的各类统计特征'''
-    df_feats, df_feats_names = [], []
+def compute_feature_engineering_single_kpi(df):
+    '''以Stream的形式给定KPI曲线的各类统计特征'''
 
     # 基础元信息记录
     # ----------------
-    timestamp_array = df['timestamp'].values
-    sensor_array = df['value'].values
-    min_interval_minutes = int(np.min(np.diff(timestamp_array))) * 60
+    df = df[['kpi_id', 'timestamp', 'value', 'label']]
+    min_interval = int(np.min(np.diff(df['timestamp'].values)))
 
-    # 滑动窗口统计量
+    # 数据入队 + 特征抽取
     # ----------------
+    stream_deque = StreamDeque(
+        interval=min_interval,
+        max_time_span=int(7 * 24 * 3600)  # 7 days
+    )
 
-    # 计算滑窗均值
-    # *************
-    for window_minutes in [1, 2, 3, 6, 10, 16, 32, 60, 120, 240, 360, 720, 1440]:
-        window_seconds = int(window_minutes * 60)
+    total_stat_feats = []
+    for data_pack in df.values:
+        tmp_feats = []
 
-        df_feats.append(
-            njit_sliding_window_mean(
-                timestamp_array, sensor_array, window_size=window_seconds
-            ).reshape(-1, 1)
-        )
-        df_feats_names.append('window_mean_last_{}'.format(window_minutes))
+        # 解压数据值
+        # *************
+        kpi_id, timestep, sensor_val, label = data_pack
+        tmp_feats.extend([kpi_id, timestep, label])
 
-    for window_minutes in [1440 + 360 * i for i in range(1, 8)]:
-        window_seconds = int(window_minutes * 60)
+        # 数据入队
+        # *************
+        stream_deque.push(timestep, sensor_val)
 
-        df_feats.append(
-            njit_sliding_window_mean(
-                timestamp_array, sensor_array, window_size=window_seconds
-            ).reshape(-1, 1)
-        )
-        df_feats_names.append('window_mean_last_{}'.format(window_minutes))
+        # 统计特征抽取
+        # *************
 
-    # 计算滑窗标准差
-    # *************
-    for window_minutes in [1, 2, 3, 6, 10, 16, 32, 60, 120, 240, 360, 720, 1440]:
-        window_seconds = int(window_minutes * 60)
+        # 计算滑窗均值
+        for window_minutes in [5, 10, 16, 32, 60, 120, 240, 360, 720, 1440]:
+            tmp_feats.append(
+                stream_deque.get_window_mean(int(window_minutes * 60))
+            )
 
-        df_feats.append(
-            njit_sliding_window_std(
-                timestamp_array, sensor_array, window_size=window_seconds
-            ).reshape(-1, 1)
-        )
-        df_feats_names.append('window_std_last_{}'.format(window_minutes))
+        # 计算滑窗标准差
+        for window_minutes in [5, 10, 16, 32, 60, 120, 240, 360, 720, 1440]:
+            tmp_feats.append(
+                stream_deque.get_window_std(int(window_minutes * 60))
+            )
 
-    # 计算滑窗EWMA
-    # *************
+        # value shift
+        for n_shift in [i for i in range(256)]:
+            tmp_feats.append(
+                stream_deque.get_window_shift(n_shift)
+            )
 
+        # 特征保存
+        # *************
+        total_stat_feats.append(tmp_feats)
 
-    # 计算滑窗Min, Max
-    # *************
-
-
-    # 计算滑窗ARIMA/AR
-    # *************
-
-    # value shift特征
-    # ----------------
-
-    # 短时shift
-    # *************
-    for shift_minutes in [i * min_interval_minutes for i in range(0, 512, 4)]:
-        df_feats.append(
-            njit_time_shift(
-                timestamp_array, sensor_array, last_seconds=shift_minutes * 60
-            ).reshape(-1, 1)
-        )
-        df_feats_names.append('shift_last_{}'.format(shift_minutes))
+        # 更新deque
+        # *************
+        stream_deque.update()
 
     # 特征组装
     # ----------------
-    df_feats = np.hstack(df_feats)
+    df_feats_names = ['kpi_id', 'timestamp', 'label'] + \
+        ['feat_{}'.format(i) for i in range(len(tmp_feats) - 3)]
+
+    df_feats = np.vstack(total_stat_feats)
     df_feats = pd.DataFrame(df_feats, columns=df_feats_names)
 
     df_feats['kpi_id'] = df['kpi_id'].values
